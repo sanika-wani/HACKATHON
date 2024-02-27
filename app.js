4823b7c917acdb9189b596e342aff0a99f8acb17
sanika-wani <wanisanika2004@gmail.com>
2024-02-27 14:01:19 +0530

    route empty folders

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
  const express = require('express'); 
-  
-const app = express(); 
-const PORT = 3000; 
-  
-app.get('/', (req, res)=>{ 
-    res.status(200); 
-    res.send("Welcome to root URL of Server"); 
-}); 
-  
+ const app = express(); 
+ const PORT = 3000; 
+ 
+const workoutRouter = require('./routes/workout')
+const challengesRouter = require('./routes/challenges')
+const profileRouter = require('./routes/profile')
+const loginRouter = require('./routes/login')
+const newtrendsRouter = require('./routes/newtrends')
+
+app.get('/' , (req,res) => {
+    res.sendFile(path.join(__dirname,"./public/index.html"));
+});
+
+app.use('/workout' , workoutRouter);
+app.use('/challenges', challengesRouter);
+app.use('/profile' , profileRouter);
+app.use('/login' , loginRouter);
+app.use('/newtrends',newtrendsRouter);
+
 app.listen(PORT, (error) =>{ 
     if(!error) 
         console.log("Server is Successfully Running,and App is listening on port "+ PORT) 
